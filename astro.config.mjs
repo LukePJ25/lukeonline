aa69557f350ee240e50a00440f8d153bcf70f094
LukeJ <luke.jones.2567@gmail.com>
2026-07-15 18:20:30 +0100

    chore: disable trailing commas

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
 	site: "https://www.lukeonline.com",
 	base: "/",
-	trailingSlash: "always",
+	trailingSlash: "never",
 });
