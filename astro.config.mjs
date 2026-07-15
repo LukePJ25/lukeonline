886463ca9b75d66abb42f8941fd7132a282f2f9b
LukeJ <luke.jones.2567@gmail.com>
2026-07-15 18:09:33 +0100

    chore: config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,7 @@
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+export default defineConfig({
+	site: "https://www.lukeonline.com",
+	base: "/",
+	trailingSlash: "always",
+});
