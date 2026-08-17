aa657797eaf91938cf0e717fdbe898054ccfeb25
Luke Jones <luke.jones.2567@gmail.com>
2026-08-17 21:19:19 +0100

    refactor: draw out shared button styling into constant

--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,6 @@
 export const SITE_TITLE = 'lukeonline'
+
+export const BUTTON_STYLING_SHARED =
+  'border-solid border-1 border-gray-300' +
+  ' bg-gray-50 flex gap-1 p-2 hover:text-blue-500' +
+  ' hover:underline focus:bg-blue-50 focus:rounded hover:bg-gray-200'
