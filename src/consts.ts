b747550ea6d4a10c7d24abc5edd7156aeb8b1343
Luke Jones <luke.jones.2567@gmail.com>
2026-08-17 21:59:41 +0100

    refactor: style changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,16 @@
 export const SITE_TITLE = 'lukeonline'
 
+const BUTTON_BACKGROUND_GRADIENT = 'bg-linear-to-t from-[#f1f1f3] to-[#f7f7f8]'
+const BUTTON_HOVER_GRADIENT = 'hover:from-[#dce6f4] hover:to-[#f1f4fa]'
+const BUTTON_ACTIVE_GRADIENT = 'active:from-[#dce6f4] active:to-[#f1f4fa]'
+const BUTTON_ACTIVE_INSET =
+  'active:inset-shadow-[0_2px_4px_-2px_rgba(0,0,0,0.6)]'
+
 export const BUTTON_STYLING_SHARED =
-  'border-solid border-1 border-gray-300' +
-  ' bg-gray-50 flex gap-1 p-2 hover:text-blue-500' +
-  ' hover:underline focus:bg-blue-50 focus:rounded hover:bg-gray-200'
+  'border-solid border-1 border-gray-400' +
+  ' flex gap-1 p-2 rounded-sm' +
+  ` ${BUTTON_BACKGROUND_GRADIENT} shadow-sm` +
+  ` ${BUTTON_HOVER_GRADIENT} hover:shadow-md hover:border-sea-600` +
+  ` ${BUTTON_ACTIVE_GRADIENT} active:shadow-none ${BUTTON_ACTIVE_INSET}` +
+  ' transition duration-150' +
+  ' active:duration-50'
